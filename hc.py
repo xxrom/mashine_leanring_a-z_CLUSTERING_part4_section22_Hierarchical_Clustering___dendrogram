4745825bc0e04e80857c7818c92ea49cff8c7324
Nikita Chernyshov <xpom55@mail.ru>
2018-03-20 19:00:52 +0300

    read data about Mall_Customers and create Dendrogram. Interesting, best number of clusters === 5

--- hc.py
+++ hc.py
@@ -1,48 +1,35 @@
 # Hierarchical Clustering
+# класторизация используется если мы не знаем какие есть группы
+# такая же кластеризация как и kmeans, только мы запускаем его один раз
+# смотрим дендрограмму и по набольшему вертикальному отрезку можно понять
+# где нужно резать все остальные вертикальные линии
+# вертикальный отрезок нужно брать максимальный самый без пересечений
+# с горизонтальными линиями
+
+# алгоритм класторизации очень простой:
+# берем количество кластеров равное N (количество точек)
+# потом объединяем две самые близкие точки(кластеры) из всех в один кластер
+# потом ищем дальше самые ближайшие кластеры между собой (4 типа расстояний)
+# и опять объединяем и так до тех пор пока не получится только один кластер
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Importing the mall dataset with pandas
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
-
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X = dataset.iloc[:, [3, 4]].values # income, spend rate [0-100]
 
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+dendrogram = sch.dendrogram(
+    sch.linkage(
+        X,
+        method = 'ward' # ??? не особо понял что это (l144)
+      )
+  )
 plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean distances')
-plt.show()
-
-# Fitting Hierarchical Clustering to the dataset
-from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
-y_hc = hc.fit_predict(X)
-
-# Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
-plt.legend()
-plt.show()
+plt.show() # дендрограмма, рисуем и находим, что 5 - количество кластеров
