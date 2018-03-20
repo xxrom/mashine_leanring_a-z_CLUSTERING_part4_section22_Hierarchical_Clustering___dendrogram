7bce262606aed8aab098ae2d9d0c79f0664648b5
Nikita Chernyshov <xpom55@mail.ru>
2018-03-20 19:18:42 +0300

    init AgglomerativeClustering class, init new object with 5 clusters, predict y_hc by hc model and visualise results

--- hc.py
+++ hc.py
@@ -33,3 +33,74 @@ plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean distances')
 plt.show() # дендрограмма, рисуем и находим, что 5 - количество кластеров
+
+# Fitting hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering( # initicalization
+    n_clusters = 5, # количество кластеров
+    affinity = 'euclidean',# расстояние какое
+    linkage = 'ward' # минимизация вариантов каждого кластера
+  )
+y_hc = hc.fit_predict(X) # predict same as k-Means
+
+# Visualising the clusters same as in kmeans without centroids
+plt.scatter(
+    X[y_hc == 0, 0], # указали условие выборки X для 1 кластера (0)
+    X[y_hc == 0, 1], # указали условие выборки Y для 1 кластера (0)
+    s = 100, # вес кисти
+    c = 'red',
+    label = 'Careful'
+  )
+plt.scatter(
+    X[y_hc == 1, 0], # указали условие выборки X для 2 кластера (1)
+    X[y_hc == 1, 1], # указали условие выборки Y для 2 кластера (1)
+    s = 100, # размер кластера
+    c = 'blue',
+    label = 'Standard'
+  )
+plt.scatter(
+    X[y_hc == 2, 0], # указали условие выборки X для 3 кластера (2)
+    X[y_hc == 2, 1], # указали условие выборки Y для 3 кластера (2)
+    s = 100, # вес кисти
+    c = 'green',
+    label = 'Target'
+  )
+plt.scatter(
+    X[y_hc == 3, 0], # указали условие выборки X для 4 кластера (3)
+    X[y_hc == 3, 1], # указали условие выборки Y для 4 кластера (3)
+    s = 100, # вес кисти
+    c = 'cyan',
+    label = 'Careless'
+  )
+plt.scatter(
+    X[y_hc == 4, 0], # указали условие выборки X для 5 кластера (4)
+    X[y_hc == 4, 1], # указали условие выборки Y для 5 кластера (4)
+    s = 100, # вес кисти
+    c = 'magenta',
+    label = 'Sensible'
+  )
+plt.title('Clusters of clients')
+plt.xlabel('Annual Incone (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
